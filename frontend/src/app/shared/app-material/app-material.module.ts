59934e797944127c14d3474f4083e11e52c996ac
Cristian Correia <cristiancorreia@DESKTOP-CP3OF22.localdomain>
2022-02-06 19:36:09 -0300

    feat(Pipe): Adicioanndo pipe para tratamento de dados

--- frontend/src/app/shared/app-material/app-material.module.ts
+++ frontend/src/app/shared/app-material/app-material.module.ts
@@ -5,6 +5,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
@@ -15,6 +16,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatDialogModule,
     MatButtonModule,
+    MatIconModule,
   ],
 })
 export class AppMaterialModule {}
